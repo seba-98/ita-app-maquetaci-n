fca43be8eba13a4a812b390abc1fae6c7299588e
sebastian belettieri <sebastianbelettieri1@gmail.com>
2022-07-20 09:52:45 -0300

    Ultimos cambios en hero-item nosotros

---
+++ ita/static/js/searchNav.js
@@ -0,0 +1,32 @@
+
+let openResp = document.getElementById('resp-search-btn');
+let closeResp = document.getElementById('resp-search-close');
+let openView = document.getElementById('view-search-btn');
+let closeView = document.getElementById('view-search-close');
+
+openResp.addEventListener('click', ()=>{
+    closeResp.style.display='block'
+    closeView.style.display='block'
+    openResp.style.display='none'
+    openView.style.display='none'
+})
+closeResp.addEventListener('click', ()=>{
+    closeResp.style.display='none'
+    openResp.style.display='block'
+    closeView.style.display='none'
+    openView.style.display='block'
+})
+
+
+openView.addEventListener('click', ()=>{
+    closeResp.style.display='block'
+    closeView.style.display='block'
+    openResp.style.display='none'
+    openView.style.display='none'
+})
+closeView.addEventListener('click', ()=>{
+    closeResp.style.display='none'
+    openResp.style.display='block'
+    closeView.style.display='none'
+    openView.style.display='block'
+})
