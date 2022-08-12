e02b9507c7ee067e975ac27dad2f565cb711f28c
sebastian belettieri <sebastianbelettieri1@gmail.com>
2022-08-12 10:43:46 -0300

    hide buttons

--- ita/static/js/searchNav.js
+++ ita/static/js/searchNav.js
@@ -1,32 +1,41 @@
 
-let openResp = document.getElementById('resp-search-btn');
-let closeResp = document.getElementById('resp-search-close');
-let openView = document.getElementById('view-search-btn');
-let closeView = document.getElementById('view-search-close');
 
-openResp.addEventListener('click', ()=>{
-    closeResp.style.display='block'
-    closeView.style.display='block'
-    openResp.style.display='none'
-    openView.style.display='none'
-})
-closeResp.addEventListener('click', ()=>{
-    closeResp.style.display='none'
-    openResp.style.display='block'
-    closeView.style.display='none'
-    openView.style.display='block'
-})
+// let openResp = document.getElementById('resp-search-btn');
+// let closeResp = document.getElementById('resp-search-close');
+// let openView = document.getElementById('view-search-btn');
+// let closeView = document.getElementById('view-search-close');
+
+// openResp.style.display='none'
+// openView.style.display='none'
+// closeView.style.display='none'
+// closeResp.style.display='none'
 
 
-openView.addEventListener('click', ()=>{
-    closeResp.style.display='block'
-    closeView.style.display='block'
-    openResp.style.display='none'
-    openView.style.display='none'
-})
-closeView.addEventListener('click', ()=>{
-    closeResp.style.display='none'
-    openResp.style.display='block'
-    closeView.style.display='none'
-    openView.style.display='block'
-})
+
+
+// openResp.addEventListener('click', ()=>{
+//     closeResp.style.display='block'
+//     closeView.style.display='block'
+//     openResp.style.display='none'
+//     openView.style.display='none'
+// })
+// closeResp.addEventListener('click', ()=>{
+//     closeResp.style.display='none'
+//     openResp.style.display='block'
+//     closeView.style.display='none'
+//     openView.style.display='block'
+// })
+
+
+// openView.addEventListener('click', ()=>{
+//     closeResp.style.display='block'
+//     closeView.style.display='block'
+//     openResp.style.display='none'
+//     openView.style.display='none'
+// })
+// closeView.addEventListener('click', ()=>{
+//     closeResp.style.display='none'
+//     openResp.style.display='block'
+//     closeView.style.display='none'
+//     openView.style.display='block'
+// })
